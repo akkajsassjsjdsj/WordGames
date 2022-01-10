2f8ad5739ee460e256b00fd28d3b3ddfd658728a
ChiPinWei <cpw13975527812@qq.c0m>
2022-01-10 11:32:09 +0800

    Update Program.cs

--- WordGames/Program.cs
+++ WordGames/Program.cs
@@ -6,7 +6,7 @@ namespace WordGames
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello ChiPinWei! Biu~Biu~Biu~");
         }
     }
 }
